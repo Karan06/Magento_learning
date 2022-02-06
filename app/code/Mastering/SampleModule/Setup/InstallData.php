ef4a2cc01cd539fde1b4caa4a28c295ae1e60922
karangiri <karangiri77@gmail.com>
2022-02-06 23:59:28 +0530

    karan new

---
+++ app/code/Mastering/SampleModule/Setup/InstallData.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * project - InstallData.php
+ *
+ * Initial Version By: karangiri
+ * Initial Version Created On: 06/02/22 11:38 PM
+ */
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Mastering\SampleModule\Setup;
+
+class InstallData implements InstallDataInterface
+{
+    /**
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $setup->getConnection()->insert(
+            $setup->getTable('mastering_sample_item'),
+            [
+                'name' => 'Item 1'
+            ]
+        );
+
+        $setup->getConnection()->insert(
+            $setup->getTable('mastering_sample_item'),
+            [
+                'name' => 'Item 2'
+            ]
+        );
+
+        $setup->endSetup();
+    }
+}
